3c2950bb18cbac352c75e855a8f7bd0bb7aa603b
StefanVaskovic-SD <stefan.vaskovic@studiodirection.com>
2026-02-23 15:44:32 +0100

    .

--- src/pages/ExchangeFunds.tsx
+++ src/pages/ExchangeFunds.tsx
@@ -208,7 +208,7 @@ export const ExchangeFunds: React.FC = () => {
                         <span className="text-sm font-normal">{toCurrency} ({CURRENCY_INFO[toCurrency].symbol})</span>
                       </>
                     ) : (
-                      <span className="text-sm font-normal text-[#716860]">Select currency</span>
+                      <span className="text-sm font-normal">Select currency</span>
                     )}
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m6 9 6 6 6-6"/></svg>
                   </button>
