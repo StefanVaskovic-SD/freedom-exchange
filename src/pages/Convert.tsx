9b677ea7e392c629fd97241441ccaea08485d9ad
StefanVaskovic-SD <stefan.vaskovic@studiodirection.com>
2026-02-26 10:39:44 +0100

    .

--- src/pages/Convert.tsx
+++ src/pages/Convert.tsx
@@ -316,7 +316,7 @@ const Convert: React.FC = () => {
 											<span className="text-sm font-normal">{toCurrency} ({CURRENCY_INFO[toCurrency].symbol})</span>
 										</>
 									) : (
-										<span className="text-sm font-normal text-[#716860]">Select currency</span>
+										<span className="text-sm font-normal">Select currency</span>
 									)}
 									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m6 9 6 6 6-6"/></svg>
 								</button>
